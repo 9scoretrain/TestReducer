a52628dd35d80c8dd3a5df1a7c59c677919cfbaa
Limin Deng <limin.deng@anu.edu.au>
2020-11-30 15:17:38 +1100

    Initial Commit

---
+++ TestReducer/TestReducerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReducerApp.swift
+//  TestReducer
+//
+//  Created by dph on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct TestReducerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
